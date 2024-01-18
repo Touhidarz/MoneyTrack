3ceb8a821ab88e77f594581bf41aeca4d01a027f
Touhid Tamboli <touhidtamboli0314@gmail.com>
2024-01-18 12:34:57 +0530

    Updated

---
+++ wallet/api/models/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const Transaction = require('./models/Transaction.js');
+const mongoose = require('mongoose');
+const app = express();
+const port = 4040;
+
+mongoose.connect("mongodb+srv://money-track:OOTTVrLNPVv3YoQF@cluster0.fw3r7eb.mongodb.net/")
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/.env', (req, res) => {
+  res.json({ message: 'test ok' });
+});
+
+app.post('/api/posttransaction', async (req, res) => {
+  try {
+    const { name, price, description, datetime } = req.body;
+    const transaction = new Transaction ({ 
+      name:name,
+      price:price, 
+      description:description, 
+      datetime:datetime 
+    });
+    console.log('Transaction created:', transaction);
+    await transaction.save();
+
+  } catch (error) {
+    console.error('Error creating transaction:', error);
+    res.status(500).json({ error: error.message });
+  }
+});
+ 
+  
+
+app.get('/api/gettransactions', async (req, res) => {
+  try {
+    const transactions = await Transaction.find();
+    console.log('Fetched transactions:', transactions);
+    res.json(transactions);
+  } catch (error) {
+    console.error('Error fetching transactions:', error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
